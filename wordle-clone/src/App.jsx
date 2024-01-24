fcf90e687574f53764aeb1585bb44d7a55623363
RhythmS00d <rhythmsood1@gmail.com>
2024-01-24 17:43:57 +1100

    feat: added typing handler and button handler

--- wordle-clone/src/App.jsx
+++ wordle-clone/src/App.jsx
@@ -5,22 +5,38 @@ import words from "./data/allWords";
 import { WordleInputGroup } from "./components/WordleInputGroup";
 import { Keyboard } from "./components/keyboard";
 
-function App() {
-  const [word, setWord] = useState("");
+// utils imports
+import { handleTyping } from "./utils/handleTyping";
 
+function App() {
   useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key.match(/^[a-zA-Z]{1}$/) || e.key === "Backspace") {
+        handleTyping(e.key.toUpperCase());
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown, true);
+
     const random = Math.floor(Math.random() * words.length - 1);
-    setWord(words[random]);
+    sessionStorage.setItem("currentValue", "");
+    sessionStorage.setItem("answer", words[random]);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown, true);
+    };
   }, []);
 
   return (
     <main className="min-w-dvh min-h-dvh my-0 bg-[#121213]">
       <nav className="w-[100dvw] flex items-center justify-center flex-nowrap m-0 p-0 border-b-[1px] border-b-[#3a3a3c] h-[65px]">
-        <h1 className="text-white text-4xl font-[nyt-karnakcondensed] font-[700] flex-grow-2">Wordle</h1>
+        <h1 className="text-white text-4xl font-[nyt-karnakcondensed] font-[700] flex-grow-2">
+          Wordle
+        </h1>
       </nav>
       <section className="h-full flex flex-col flex-1 items-center justify-evenly">
         <section className="flex flex-col items-center gap-[5px] mt-6 mb-6">
-          <WordleInputGroup multiplier={5} value="A" />
+          <WordleInputGroup multiplier={5} value={"abcde".toUpperCase()} />
           <WordleInputGroup multiplier={5} value="A" />
           <WordleInputGroup multiplier={5} value="A" />
           <WordleInputGroup multiplier={5} value="A" />
--- wordle-clone/src/components/WordleInputGroup/WordleInputGroup.jsx
+++ wordle-clone/src/components/WordleInputGroup/WordleInputGroup.jsx
@@ -1,13 +1,14 @@
 import { WordleInput } from "../wordleInput/wordleInput";
 import { cn } from "../../utils/twMerge";
 
-export const WordleInputGroup = ({ multiplier, className, ...props }) => {
+export const WordleInputGroup = ({ multiplier, value, className, ...props }) => {
+
   return (
     <div className={cn("flex gap-[5px]", className)}>
       {Array(multiplier)
         .fill(0)
         .map((input, index) => (
-          <WordleInput key={index} {...props} />
+          <WordleInput key={index} {...props} value={value[index]}/>
         ))}
     </div>
   );
