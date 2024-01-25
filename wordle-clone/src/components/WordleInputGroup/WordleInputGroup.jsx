aea54a99bc2b8ff06aeba8999e043568b1d82e54
RhythmS00d <rhythmsood1@gmail.com>
2024-01-25 12:38:09 +1100

    chore: added data type 'row'

--- wordle-clone/src/components/WordleInputGroup/WordleInputGroup.jsx
+++ wordle-clone/src/components/WordleInputGroup/WordleInputGroup.jsx
@@ -4,11 +4,11 @@ import { cn } from "../../utils/twMerge";
 export const WordleInputGroup = ({ multiplier, value, className, ...props }) => {
 
   return (
-    <div className={cn("flex gap-[5px]", className)}>
+    <div className={cn("flex gap-[5px]", className)} data-type="row">
       {Array(multiplier)
         .fill(0)
         .map((input, index) => (
-          <WordleInput key={index} {...props} value={value[index]}/>
+          <WordleInput key={index} {...props} value={value[index]} />
         ))}
     </div>
   );
